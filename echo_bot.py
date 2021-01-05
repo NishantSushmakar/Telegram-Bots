bf75652f603e6c3e0dca3953ce8f63d128068ba4
NishantSushmakar <46222988+NishantSushmakar@users.noreply.github.com>
2021-01-05 23:36:45 +0530

    Update echo_bot.py

--- echo_bot.py
+++ echo_bot.py
@@ -10,7 +10,7 @@ from telegram.ext import Updater,CommandHandler,MessageHandler,Filters
 logging.basicConfig(format='%(asctime)s-%(name)s-%(levelname)s-%(message)s',level=logging.INFO)
 logger= logging.getLogger(__name__)
 
-TOKEN = '1569620183:AAH8m4pRnxxtoi3VbZurEJxIbYPO2qYRf10'
+TOKEN = your_token_id
 
 def start(update, context):
     print(update)
